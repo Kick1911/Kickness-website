5c48e24b88355a9b03c8e0abe701237dce02a062
Kickness <caswallengelsman6@gmail.com>
2019-09-07 23:55:26 +0200

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="main">
+    <h1>I am Kick</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+#main{
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  background-color: rgba(0,0,0,0.5);
+  h1{
+    margin-top:15%;
+  }
+}
+</style>
